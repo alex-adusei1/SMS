660313c79e4cd3e127089282fe85c600cee9484e
Alex Adusei <alex.a.adusei@gmail.com>
2017-09-15 15:16:12 +0000

    username seeder

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class UserTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,16 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-//         $this->call(UsersTableSeeder::class);
         DB::table('users')->insert([
             'username' => 'admin',
             'email' => 'admin@gmail.com',
             'password' => bcrypt('admin@123'),
-            'residential_address' => 'Accra',
-            'mobile'=>'0244000000', 
-            'city'=>'dansoman', 
-            'place_of_birth'=>'accra',
-            'tribe'=>'Ga',
         ]);
     }
 }
